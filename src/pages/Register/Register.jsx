666eed1445c123de0df01f97f9a07c607d45f179
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-05-05 12:01:38 +0600

    register added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import InputField from '../../components/InputField/InputField';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+    const handleRegister = (e) => {
+        // axios
+        e.preventDefault();
+        // console.log(e.target.name.value);
+        axios.post('http://localhost:5000/register', {
+            email: e.target.email.value,
+            name: e.target.name.value,
+            password: e.target.password.value,
+        }).then(res => console.log(res))
+    }
+
     return (
         <div className='h-screen w-screen flex items-center justify-center absolute top-0 z-0'>
             <div className='w-11/12 lg:w-1/3 p-[25px] shadow-[3px_4px_10px_2px_rgba(0,0,0,0.05)] rounded-[10px]'>
                 <h4 className="text-heading-4 text-center">Register</h4>
 
-                <form className="mt-[37px]">
-                    <InputField label="Name" id='name' page='register' />
-                    <InputField label="Email" id='email' page='register' />
-                    <InputField label="Password" id='password' page='register' />
+                <form onSubmit={handleRegister} className="mt-[37px]">
+                    <InputField name='name' label="Name" id='name' page='register' />
+                    <InputField name='email' label="Email" id='email' page='register' />
+                    <InputField name='password' label="Password" id='password' page='register' />
                     <InputField type='submit' className='w-full bg-dark mt-[18px] py-[13px] rounded-[8px] text-dark-200' value="LOGIN" />
                 </form>
 
