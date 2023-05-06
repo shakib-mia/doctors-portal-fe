6e3b224d6d752bcd97fe55543311fcd9b0372988
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-05-06 15:33:14 +0600

    appointments added

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,66 @@
 export const url = 'http://localhost:5000/'
+
+
+const arr = [
+    {
+        "_id": "628098b9fc211033656b0293",
+        "treatmentId": 3,
+        "treatmentName": "Teeth Cleaning",
+        "date": "May 15, 2022",
+        "slot": "08.00 AM - 08.30 AM",
+        "patient": "smdshakibmia2001@gmail.com",
+        "patientName": "Md. Shakib Mia"
+    },
+    {
+        "_id": "6280dd831f7185f818e860b8",
+        "treatmentId": 1,
+        "treatmentName": "Teeth Orthodontics",
+        "date": "May 15, 2022",
+        "slot": "08.00 AM - 08.30 AM",
+        "patient": "smdshakibmia2001@gmail.com",
+        "patientName": "Md. Shakib Mia"
+    },
+    {
+        "_id": "6280ddae1f7185f818e860b9",
+        "treatmentId": 6,
+        "treatmentName": "Oral Surgery",
+        "date": "May 15, 2022",
+        "slot": "08.00 AM - 08.30 AM",
+        "patient": "smdshakibmia2001@gmail.com",
+        "patientName": "Md. Shakib Mia"
+    },
+    {
+        "_id": "6280e0231f7185f818e860ba",
+        "treatmentId": 2,
+        "treatmentName": "Cosmetic Dentistry",
+        "date": "May 15, 2022",
+        "slot": "2.00 PM - 2.30 PM",
+        "patient": "smdshakibmia2001@gmail.com",
+        "patientName": "Md. Shakib Mia"
+    },
+    {
+        "_id": "6281ca22d451770a9802d8d6",
+        "treatmentId": 2,
+        "treatmentName": "Cosmetic Dentistry",
+        "date": "May 16, 2022",
+        "slot": "08.00 AM - 08.30 AM",
+        "patient": "mdaasshakibmia2001@gmail.com",
+        "patientName": null
+    },
+    {
+        "_id": "6281daaed451770a9802d8d7",
+        "treatmentId": 4,
+        "treatmentName": "Cavity Protection",
+        "date": "May 16, 2022",
+        "slot": "08.00 AM - 08.30 AM"
+    },
+    {
+        "_id": "628267aad6c17d48a58f6115",
+        "treatmentId": 3,
+        "treatmentName": "Teeth Cleaning",
+        "date": "May 16, 2022",
+        "slot": "1.30 PM - 2.00 PM",
+        "patient": "smdshakibmia2001@gmail.com",
+        "patientName": "Md. Shakib Mia"
+    }
+]
--- src/pages/Appointments/Appointments.jsx
+++ src/pages/Appointments/Appointments.jsx
@@ -2,11 +2,13 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { url } from '../../constants';
 import AppointmentCard from '../../components/AppointmentCard/AppointmentCard';
-// import Card from '../../components/Card/Card';
+import Calendar from 'react-calendar';
+import chair from './../../assets/images/chair.webp'
 
 const Appointments = () => {
 
-    const [appointments, setAppointments] = useState([])
+    const [appointments, setAppointments] = useState([]);
+    // const [date, setDate] = useState(new Date())
 
     useEffect(() => {
         const config = {
@@ -17,14 +19,24 @@ const Appointments = () => {
 
         axios.get(url + 'services', config).then(res => {
             setAppointments(res.data.services)
-            console.log(res.data);
+            console.log(res.data.services);
         })
     }, [])
     return (
-        <div className='p-[5rem] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-            {/* appointments : {appointments.length} */}
-            {appointments.map((props) => <AppointmentCard {...props} />)}
-        </div>
+        <>
+            <div className='flex gap-[21px] px-[160px] justify-between items-center'>
+                <div className='w-1/2'>
+                    <Calendar onChange={e => console.log(e)} className='w-1/2 mx-auto bg-white p-[20px] shadow-[3px_4px_10px_2px_rgba(0,0,0,0.05)] rounded-[18px]' />
+                </div>
+                <div className='w-1/2'>
+                    <img src={chair} className='' alt="" />
+                </div>
+            </div>
+            <div className='p-[5rem] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {/* appointments : {appointments.length} */}
+                {Array.isArray(appointments) ? appointments.map((props, id) => <AppointmentCard key={id} {...props} />) : <AppointmentCard _id={appointments.id} name={appointments.name} slots={appointments.slots} />}
+            </div>
+        </>
     );
 };
 
